aa57b301280c5eab398880cb7e170fe6be8fcb61
Kira Figg <kira.figg@landregistry.gsi.gov.uk>
2014-11-19 18:02:20 +0000

    add files

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#apt-get update
+#apt-get install python
+#apt-get install -y python-pip
+#pip install Flask
+apt-get install -y jruby
+#apt-get install -y ruby rubygems
+gem install sinatra
+
+
+
